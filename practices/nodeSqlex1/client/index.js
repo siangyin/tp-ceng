80ad93c62c42ca7e04bf61e6122814a47252bec5
Lee Siang Yin <siangyin916@gmail.com>
2023-05-14 21:07:27 +0800

    connecting frontend with server, setup dotenv

--- practices/nodeSqlex1/client/index.js
+++ practices/nodeSqlex1/client/index.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+	fetch("http://localhost:5000/getAll")
+		.then((res) => res.json())
+		.then((data) => console.log(data));
 	loadHtmlTable([]);
 });
 

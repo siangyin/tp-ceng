d0cdf1f31ea165db36ce03b7ff14ee6ae575fd13
Lee Siang Yin <siangyin916@gmail.com>
2023-05-14 21:51:48 +0800

    troubleshoot for mysql connection

--- practices/nodeSqlex1/server/server.js
+++ practices/nodeSqlex1/server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const dbService = require("./dbService");
 app.use(cors()); // to be able to send data to backend
 app.use(express.json()); // able to ssend json format
 app.use(express.urlencoded({ extended: false })); // false wont be sending any form data and if true will to parse the incoming request with urlencoded payloads and is based upon the body-parser.

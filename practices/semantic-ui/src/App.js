a0ddab696b0a8339c0aa70a976967da5399339f5
Lee Siang Yin <siangyin916@gmail.com>
2023-05-14 23:07:41 +0800

    semantic ui draft

---
+++ practices/semantic-ui/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
+function App() {
+	return (
+		<div className="App">
+			<NavBar />
+
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ practices/semantic-ui/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+	return (
+		<div class="ui center aligned header">
+			<div class="ui celled horizontal list">
+				<div class="item">DineAdvisor</div>
+				<div class="item">About Us</div>
+				<div class="item">Contact</div>
+			</div>
+		</div>
+	);
+};
+export default Footer;

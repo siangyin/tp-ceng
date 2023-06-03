2669e4b77e3eba41b2315ee2c7a5af25da341b71
Lee Siang Yin <siangyin916@gmail.com>
2023-06-04 00:13:08 +0800

    created register and login controller

--- practices/webad1api/controllers/authController.js
+++ practices/webad1api/controllers/authController.js
@@ -1,34 +1,55 @@
 const pool = require("../database/connectSql2");
-// const mysql = require("../database/connectDb");
-// const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const createJWT = (payload) =>
+	(token = jwt.sign(payload, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	}));
 
 const register = async (req, res) => {
 	try {
 		const { email, password, username } = req.body;
-		const [results] = await pool.query("select * from users where email = ?", [
+
+		// find users with same email
+		const [users] = await pool.query("select * from users where email = ?", [
 			email,
 		]);
-		if (results[0])
+		// if found users with same email
+		if (users.length) {
 			return res
 				.status(400)
 				.json({ status: "Bad request", msg: "Email already exists" });
-
-		const sql =
-			"insert into users (email, password, username) values (?, ?, ?)";
-		const [rows, fields] = await pool.query(sql, [email, password, username]);
-
-		if (rows.affectedRows) {
-			return res
-				.status(200)
-				.json({ status: "OK", msg: "user created successfully" });
 		} else {
-			return res.json({
-				msg: "error",
-			});
+			const sql =
+				"insert into users (email, password, username) values (?, ?, ?)";
+			const [user] = await pool.query(sql, [email, password, username]);
+			// rows.insertedId is the created item id
+			// new user registration success
+			if (user.insertId) {
+				const token = createJWT({
+					userId: user.insertId,
+					email,
+					username,
+					role: "user",
+				});
+
+				return res.status(201).json({
+					status: "OK",
+					msg: "user created successfully",
+					token,
+				});
+			} else {
+				// new user registration failed
+				return res.status(406).json({
+					status: "Request failed",
+					msg: "error",
+				});
+			}
 		}
 	} catch (error) {
 		res.status(500).json({
-			msg: error.message,
+			status: "Server error",
+			msg: error,
 		});
 	}
 };
@@ -36,54 +57,39 @@ const register = async (req, res) => {
 const login = async (req, res) => {
 	try {
 		const { email, password } = req.body;
-		const [results] = await pool.query("select * from users where email = ?", [
+		const [users] = await pool.query("select * from users where email = ?", [
 			email,
 		]);
-		if (results[0])
+
+		// found user with email & check if password matched
+		if (users[0] && users[0].password === password) {
+			const user = {
+				userId: users[0].userId,
+				email: users[0].email,
+				username: users[0].username,
+				role: users[0].role,
+			};
+
+			const token = createJWT(user);
+
 			return res.status(200).json({
-				results: results,
+				status: "OK",
+				data: user,
+				token,
 			});
+		}
+		return res.status(401).json({
+			status: "Invalid credentials",
+		});
 	} catch (error) {
 		res.status(500).json({
-			msg: error.message,
+			status: "Server error",
+			msg: error,
 		});
 	}
 };
-
-// unable to insert with mysql pkg
-// const registerX = async (req, res) => {
-// 	const { email, password, username } = req.body;
-
-// 	let sqlCmd = `INSERT INTO Users (username, email, password) VALUES (?,?,?);'`;
-
-// 	mysql.query(sqlCmd, [username, email, password], (err, result) => {
-// 		if (err) {
-// 			throw err;
-// 		} else {
-// 			res.status(200).json(result);
-// 		}
-// 	});
-// };
-
-// able to get users table with mysql pkg
-// const login = async (req, res) => {
-// 	let sqlCmd = "select * from users";
-
-// 	mysql.query(sqlCmd, (err, result) => {
-// 		if (err) {
-// 			throw err;
-// 		} else {
-// 			res.status(200).json(result);
-// 		}
-// 	});
-// };
 
 module.exports = {
 	register,
 	login,
 };
-// login,
-// logout,
-// verifyEmail,
-// resetPassword,
-// forgotPassword,
---
+++ practices/webad1api/database/schema.sql
@@ -0,0 +1,96 @@
+-- CREATE DATABASE webad1;
+USE webad1;
+
+CREATE TABLE Users (
+userId INT(5) AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL UNIQUE,
+password VARCHAR(100) NOT NULL,
+role VARCHAR(30) NOT NULL DEFAULT 'user',
+createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+profilePhoto TEXT
+);
+
+CREATE TABLE Restaurants (
+restaurantId INT(5) AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+description TEXT,
+website VARCHAR(255),
+contact VARCHAR(20),
+add1 VARCHAR(100),
+add2 VARCHAR(100),
+add3 VARCHAR(100),
+city VARCHAR(100),
+country VARCHAR(100),
+postalCode VARCHAR(20),
+area VARCHAR(50),
+status VARCHAR(10) DEFAULT 'active'
+);
+
+CREATE TABLE Restaurant_Review (
+  reviewId INT(5) AUTO_INCREMENT PRIMARY KEY,
+  restaurantId INT(5) NOT NULL,
+  userId INT(5) NOT NULL,
+  title VARCHAR(255),
+  comment TEXT NOT NULL,
+  rating DECIMAL(2,1) NOT NULL,
+	status VARCHAR(10) DEFAULT 'active',
+  createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT FK_restaurant_review FOREIGN KEY (restaurantId)
+    REFERENCES Restaurants(restaurantId),
+	CONSTRAINT FK_user_review FOREIGN KEY (userId)
+    REFERENCES Users(userId)
+);
+
+CREATE TABLE Restaurant_Photo (
+photoId INT(5) AUTO_INCREMENT PRIMARY KEY,
+restaurantId INT(5) NOT NULL,
+reviewId INT(5),
+photoUrl TEXT NOT NULL,
+defaultPhoto BOOLEAN DEFAULT false,
+createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+addedBy INT(5) NOT NULL,
+CONSTRAINT FK_restaurant_photo FOREIGN KEY (restaurantId)
+    REFERENCES Restaurants(restaurantId),
+	CONSTRAINT FK_review_photo FOREIGN KEY (reviewId)
+    REFERENCES Restaurant_Review(reviewId),
+CONSTRAINT FK_user_photo FOREIGN KEY (addedBy)
+   REFERENCES Users(userId)
+);
+
+CREATE TABLE Opening_Hours (
+  openhrsId INT(5) AUTO_INCREMENT PRIMARY KEY,
+  restaurantId INT(5) NOT NULL,
+  dayOfWeek VARCHAR(20) NOT NULL,
+  openingTime TIME NOT NULL,
+  closingTime TIME NOT NULL,
+CONSTRAINT FK_restaurant_openhrs FOREIGN KEY (restaurantId)
+    REFERENCES Restaurants(restaurantId)
+);
+
+CREATE TABLE Category_Type (
+categoryType VARCHAR(20) NOT NULL,
+typeName VARCHAR(50) NOT NULL,
+CONSTRAINT category_type PRIMARY KEY (categoryType, typeName)
+);
+
+CREATE TABLE Restaurant_Category_Type (
+restaurantId INT(5) NOT NULL,
+categoryType VARCHAR(20),
+typeName VARCHAR(50),
+  CONSTRAINT FK_category_type
+    FOREIGN KEY (categoryType, typeName) REFERENCES Category_Type (categoryType, typeName),
+  CONSTRAINT FK_restaurant_category_type
+    FOREIGN KEY (restaurantId) REFERENCES Restaurants(restaurantId)
+);
+
+CREATE TABLE Promotion (
+promoId INT(5) AUTO_INCREMENT PRIMARY KEY,
+restaurantId INT(5) NOT NULL,
+title VARCHAR(255) NOT NULL,
+description TEXT,
+startDate DATETIME,
+endDate DATETIME,
+CONSTRAINT FK_restaurant_promo FOREIGN KEY (restaurantId)
+    REFERENCES Restaurants(restaurantId)
+);
